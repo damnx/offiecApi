1035abf1f3ea6d22c1f27a46a859ce20453c024b
damnx <damnx@fingroup.vn>
2018-07-30 17:08:30 +0700

    update

--- app/Roles.php
+++ app/Roles.php
@@ -9,13 +9,6 @@ class Roles extends Model
     protected $table = 'roles';
 
     protected $fillable = [
-<<<<<<< HEAD
-        'name', 'permissions', 'description',
-    ];
-
-    
-=======
         'name', 'permission', 'description'
     ];
->>>>>>> c8bdd501c26abd2b18090ad57522caf755475729
 }
